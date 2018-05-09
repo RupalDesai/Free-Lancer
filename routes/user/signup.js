4075b5ef4f036efaa95ef87f9ab019184a317d70
Paras Garg <pgarg2@stevens.edu>
2018-05-09 12:03:47 -0400

    user setup

---
+++ routes/user/signup.js
@@ -0,0 +1,26 @@
+/* 
+ * User Routers * 
+ * User Data Access Object *
+ * User Signup *
+*/
+
+/* importing required files and packages */
+const express = require('express');
+const router = express.Router();
+
+/* global scoped function */
+//------------------------ route to update user information by id
+router.get('/', async (req, res) => {
+    res.render('static/about-us', {
+        mainTitle: "About Us",
+        mainDescription: "Welcome to the Free Lancer | A search engine to find a best job and workspace.",
+        // user: req.user,
+        about: "Lorem ipsum dolor sit amet, ut id, vestibulum nec wisi vel scelerisque ut, et vestibulum risus enim vestibulum, vehicula ante, tortor erat non. Quis luctus, sed vestibulum urna dui eu rerum ac, nonummy commodo.",
+        contact: {
+            phone: "+1 987-654-3210",
+            email: "cs546group@stevens.edu"
+        }
+    });
+});
+// exporting routing apis
+module.exports = router;
