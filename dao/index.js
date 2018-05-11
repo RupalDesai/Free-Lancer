bf7f89e8fb8b95b729d8c54bc2752fe0c55d421b
unknown <powarnamita23@gmail.com>
2018-05-11 19:57:26 -0400

    filter functionality

--- dao/index.js
+++ dao/index.js
@@ -2,6 +2,7 @@ module.exports = {
     credentials: require('./credentials'),
     companies: require('./companies'),
     workspaces: require('./workspaces'),
+    jobFilter: require('./jobFilter'),
     workspacesReviews: require('./workspaces-reviews'),
     users: require('./user')
 };
